c5d522e4634fc73ffe2a4d3e7feec6c86d7f6b4f
balar-35 <balamuralik950@gmail.com>
2025-07-19 13:22:11 +0530

    Fix: Added Vite to devDependencies

--- client/App.tsx
+++ client/App.tsx
@@ -7,7 +7,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CartProvider } from "./contexts/CartContext";
-import Index from "./pages/Index";
+import Index from "./pages/index";
 import Menu from "./pages/Menu";
 import Cart from "./pages/Cart";
 import Contact from "./pages/Contact";
